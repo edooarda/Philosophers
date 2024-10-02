0a1c96cb9be980f775b322929a9c6289a18966f3
edooarda <dudamakesthings@gmail.com>
2024-10-02 14:09:56 +0200

    fix potential deadlock with time issues

--- routine_actions.c
+++ routine_actions.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/24 12:55:45 by edribeir      #+#    #+#                 */
-/*   Updated: 2024/10/02 12:14:09 by edribeir      ########   odam.nl         */
+/*   Updated: 2024/10/02 14:09:12 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,31 @@ static void	pickup_second_hashi(t_philo *philo, pthread_mutex_t *second_hashi)
 		write(2, "Error locking Hashi\n", 22);
 }
 
+static bool	eating_even(t_philo *philo)
+{
+	if (pthread_mutex_lock(philo->r_hashi) == 0)
+	{
+		print_message(philo, HASHI);
+		pickup_second_hashi(philo, philo->l_hashi);
+		pthread_mutex_unlock(philo->r_hashi);
+	}
+	else
+	{
+		write(2, "Error locking Hashi\n", 22);
+		return (false);
+	}
+	return (true);
+}
+
 static bool	eating(t_philo *philo)
 {
-	if (pthread_mutex_lock(philo->l_hashi) == 0)
+	if (pthread_mutex_lock(philo->r_hashi) == 0)
 	{
 		print_message(philo, HASHI);
 		if (is_single_philo(philo) == true)
 			return (false);
-		pickup_second_hashi(philo, philo->r_hashi);
-		pthread_mutex_unlock(philo->l_hashi);
+		pickup_second_hashi(philo, philo->l_hashi);
+		pthread_mutex_unlock(philo->r_hashi);
 	}
 	else
 	{
@@ -68,6 +84,10 @@ static void	sleeping_and_thinking(t_philo *philo)
 	resting(philo->table->time_to_sleep, philo);
 	print_message(philo, THINK);
 }
+// bool	simulation_stopper(t_philo *philo)
+// {
+	
+// }
 
 void	*routine(void *arg)
 {
@@ -76,15 +96,19 @@ void	*routine(void *arg)
 	philo = (t_philo *)arg;
 	pthread_mutex_lock(&philo->table->start_lock);
 	pthread_mutex_unlock(&philo->table->start_lock);
-	if ((philo->philo_id % 2) == 0)
-	{
-		print_message(philo, THINK);
-		resting(philo->table->time_to_eat / 2, philo);
-	}
 	while (1)
 	{
-		if (eating(philo) == false)
-			return (NULL);
+		if ((philo->philo_id % 2) == 0)
+		{
+			resting(philo->table->time_to_eat / 2, philo);
+			if (eating_even(philo) == false)
+				return (NULL);
+		}
+		else
+		{
+			if (eating(philo) == false)
+				return (NULL);
+		}
 		pthread_mutex_lock(&philo->table->dead_lock);
 		if (philo->table->is_alive == false
 			|| philo->nb_meals == philo->table->how_many_meals)
